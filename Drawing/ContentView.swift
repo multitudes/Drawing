7136679b39915615f7667bc00eabde71658f8d7d
laurent <wrmultitudes@gmail.com>
2019-11-06 17:29:36 +0100

    day43

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,9 +8,55 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount: CGFloat = 0
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width/2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        return path
+    }
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+//        Circle()
+//            .strokeBorder(Color.blue, lineWidth: 40)
+        
+        Arc(startAngle: .degrees(0), endAngle: .degrees(330), clockwise: true, insetAmount: 21)
+            .stroke(Color.blue, lineWidth: 10)
+        .frame(width: 300, height: 300)
+        
+        
+//        Triangle()
+//            .stroke(Color.red,style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//            .frame(width: 300, height: 300)
     }
 }
 
