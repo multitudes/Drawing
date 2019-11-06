274c08d7aca814301f4975f8a639ad7b8d7c6b30
laurent <wrmultitudes@gmail.com>
2019-11-06 18:40:54 +0100

    imagepainter

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        //Text("Hello, World!")
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("example"), scale: 0.1), lineWidth: 25)
+            .frame(width:300, height: 300)
+//        .frame(width: 300, height: 300)
+//            .border(Color.red, width: 30)
+//            .border(ImagePaint(image: Image("example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5),scale: 0.035), width: 30)
     }
 }
 
